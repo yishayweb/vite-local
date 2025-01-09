ea7f1fe816a63bfea6341f5609004f3b592198c3
yishay-at-bay <95177000+yishay-at-bay@users.noreply.github.com>
2025-01-09 11:55:22 +0200

    deleted files translations

--- src/language/i18n.ts
+++ src/language/i18n.ts
@@ -21,7 +21,7 @@ i18n
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
-    ns: ["translation", "homePage"],
+    ns: ["homePage"],
     // backend: {
     //   loadPath: "/locales/{{lng}}.json",
     // },
